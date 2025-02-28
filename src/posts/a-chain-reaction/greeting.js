9c78cec7a25208d831b2972137783bcf6114d5b0
liuyu <1093940224@qq.com>
2025-02-28 15:53:16 +0800

    删除不用的文章

--- src/posts/a-chain-reaction/greeting.js
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export function Greeting({ person }) {
-    return (
-        <p className="font-sans text-2xl text-purple-400 dark:text-purple-500">
-            Hello, <i>{person.firstName}</i>!
-        </p>
-    );
-}
